0efe1a88ed9571c6711850346e2a74d932b8bf2b
DavidTurnley <94334160+DavidTurnley@users.noreply.github.com>
2025-03-08 23:01:44 -0700

    adding in some documentation
    let's hope this all works -v-;

--- theThing.py
+++ theThing.py
@@ -1,5 +1,54 @@
 
 
 
+'''
+Okay so from what I've learned so far, the "core" just does a lot of general stuff
+
+here's where a lot of the important info is: https://noxrepo.github.io/pox-doc/html/#openflow-in-pox
+
+it is pain
+
+_handle_[eventName] is the method name for a specific event handler
+
+message stuff is here: https://noxrepo.github.io/pox-doc/html/#openflow-messages
+
+ofp_flow_mod is is the way to add/modify a rule in the switch
+
+information on the actions that can be taken from a rule are here: https://noxrepo.github.io/pox-doc/html/#openflow-actions
+
+Good luck and I bid you well
+
+'''
+
+
+
+from pox.core import core # type: ignore
+from pox.lib.util import dpid_to_str # type: ignore
+
+# use this to print stuff to the screen, there's a lot of garbage but just deal with it tbh
+log = core.getLogger()
+
+class MyComponent (object):
+    def __init__ (self):
+        core.openflow.addListeners(self)
+
+    def _handle_ConnectionUp (self, event):
+        log.debug("Switch %s has come up.", dpid_to_str(event.dpid))
+
+    def _handle_PortStatus (self, event):
+        if event.added :
+            # port was added to the switch
+            log.debug("Added port: $s", event.port)
+
+        if event.deleted:
+            # port was removed from the switch
+            log.debug("Removed port: $s", event.port)
+
+        if not event.added and not event.deleted:
+            #port was modified
+            log.debug("Modified port: %s", event.port)
+
+
 def launch():
-    print("Hello World inside the VM!")
+    core.registerNew(MyComponent)
+    log.debug("Hello World inside the VM!")
