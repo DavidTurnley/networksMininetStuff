67e630f28b76f999e3753652020e8e61af923eb0
DavidTurnley <94334160+DavidTurnley@users.noreply.github.com>
2025-03-25 12:04:28 -0600

    ugh

--- theThing.py
+++ theThing.py
@@ -26,7 +26,7 @@ from pox.core import core
 from pox.lib.util import dpid_to_str 
 from pox.lib.packet.arp import arp
 from pox.lib.packet.ethernet import ethernet, EthAddr
-from pox.lib.packet.ipv6 import IPAddr
+from pox.lib.packet.ipv4 import IPAddr
 
 import pox.openflow.libopenflow_01 as of
 
@@ -122,7 +122,7 @@ class MyComponent (object):
 
             newFlow.match.nw_dst = (IPAddr("10.0.0.10"), 32)
 
-            newFlow.match.nw_src = a.hwsrc
+            #newFlow.match.nw_src = a.hwsrc
 
             newFlow.actions.append(of.ofp_action_nw_addr.set_dst(IPAddr("10.0.0.5")))
             newFlow.actions.append(of.ofp_action_output(port = 5))
